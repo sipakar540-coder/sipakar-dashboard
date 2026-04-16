5c06dd24354c170899f757bd3d3f507f36bfbb42
sipakar540-coder <sipakar540@gmail.com>
2026-04-16 10:58:55 +0700

    Update script.js

--- SIPAKAR_Project/script.js
+++ SIPAKAR_Project/script.js
@@ -1,15 +1,15 @@
 // script.js
 
-//const sheetURL = 'https://docs.google.com/spreadsheets/d/e/2PACX-1vQLb9sJ1YIxz1r2x0K_f89iHodz9fZ0l8IzJI7pNVK9clBN7_v7RztaCK32vQbPzxAzmTyyIlbvOjFU/pub?gid=377540299&single=true&output=csv';
+const sheetURL = 'https://docs.google.com/spreadsheets/d/e/2PACX-1vQLb9sJ1YIxz1r2x0K_f89iHodz9fZ0l8IzJI7pNVK9clBN7_v7RztaCK32vQbPzxAzmTyyIlbvOjFU/pub?gid=377540299&single=true&output=csv';
 
 async function loadData() {
     try {
         const response = await fetch(sheetURL);
         const data = await response.text();
         
-        //const rows = data.split('\n').length - 1; 
+        const rows = data.split('\n').length - 1; 
         
-        //document.getElementById('total-aset').innerText = rows;
+        document.getElementById('total-aset').innerText = rows;
         
         console.log("Data SIPAKAR berhasil dimuat!");
     } catch (error) {
@@ -17,4 +17,4 @@ async function loadData() {
     }
 }
 
-//window.onload = loadData;
+window.onload = loadData;
